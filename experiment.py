d03d995de84cf40b8d5a7a3d503bb990e4e196e1
takuti <k.takuti@gmail.com>
2016-03-21 06:08:38 +0900

    pep

--- experiment.py
+++ experiment.py
@@ -13,7 +13,9 @@ PATH_TO_RATINGS = '../../data/ml-1m/ratings.dat'
 PATH_TO_MOVIES = '../../data/ml-1m/movies.dat'
 PATH_TO_USERS = '../../data/ml-1m/users.dat'
 
+
 class Runner:
+
     def __init__(self, limit=200000):
         # number of test samples
         self.limit = limit
@@ -38,7 +40,7 @@ class Runner:
         # pre-train
         model.fit(self.samples[:self.n_train])
 
-        return model.evaluate(self.samples[self.n_train:self.n_train+self.n_test])
+        return model.evaluate(self.samples[self.n_train:self.n_train + self.n_test])
 
     def biased_iMF(self):
         """Biased Incremental Matrix Factorizaton
@@ -50,7 +52,7 @@ class Runner:
         """
         model = IncrementalBiasedMF(self.n_user, self.n_item)
         model.fit(self.samples[:self.n_train])
-        return model.evaluate(self.samples[self.n_train:self.n_train+self.n_test])
+        return model.evaluate(self.samples[self.n_train:self.n_train + self.n_test])
 
     def iFMs(self, contexts=()):
         """Incremental Factorization Machines
@@ -68,7 +70,7 @@ class Runner:
         """
         model = IncrementalFMs(self.n_user, self.n_item, contexts)
         model.fit(self.samples[:self.n_train])
-        res = model.evaluate(self.samples[self.n_train:self.n_train+self.n_test])
+        res = model.evaluate(self.samples[self.n_train:self.n_train + self.n_test])
         # print auto-updated regularization params
         print model.l2_reg_w0, model.l2_reg_w, model.l2_reg_V
         return res
@@ -87,18 +89,20 @@ class Runner:
 
         self.samples = []
 
-        prev_date = datetime(*time.localtime(self.ratings[0,3])[:6])
+        prev_date = datetime(*time.localtime(self.ratings[0, 3])[:6])
 
-        head_date = datetime(*time.localtime(self.ratings[0,3])[:6])
+        head_date = datetime(*time.localtime(self.ratings[0, 3])[:6])
         self.dts = []
 
         for user_id, item_id, rating, timestamp in self.ratings:
             # give an unique user index
-            if user_id not in user_ids: user_ids.append(user_id)
+            if user_id not in user_ids:
+                user_ids.append(user_id)
             u_index = user_ids.index(user_id)
 
             # give an unique item index
-            if item_id not in item_ids: item_ids.append(item_id)
+            if item_id not in item_ids:
+                item_ids.append(item_id)
             i_index = item_ids.index(item_id)
 
             # delta days
@@ -106,7 +110,7 @@ class Runner:
             dt = self.__delta(prev_date, date)
             prev_date = date
 
-            sample = { 'u_index': u_index, 'i_index': i_index, 'dt': np.array([dt]), 'genre': movies[item_id], 'demographics': users[user_id] }
+            sample = {'u_index': u_index, 'i_index': i_index, 'dt': np.array([dt]), 'genre': movies[item_id], 'demographics': users[user_id]}
 
             self.samples.append(sample)
             self.dts.append(self.__delta(head_date, date))
@@ -114,8 +118,8 @@ class Runner:
         self.n_user = len(user_ids)
         self.n_item = len(item_ids)
         self.n_sample = len(self.samples)
-        self.n_train = int(self.n_sample * 0.2) # 20% for pre-training to avoid cold-start
-        self.n_test = min(self.n_sample-self.n_train, self.limit)
+        self.n_train = int(self.n_sample * 0.2)  # 20% for pre-training to avoid cold-start
+        self.n_test = min(self.n_sample - self.n_train, self.limit)
 
     def __load_movies(self):
         """Load movie genres as a context.
@@ -128,23 +132,23 @@ class Runner:
             lines = map(lambda l: l.rstrip().split('::'), f.readlines())
 
         all_genres = ['Action',
-                'Adventure',
-                'Animation',
-                "Children's",
-                'Comedy',
-                'Crime',
-                'Documentary',
-                'Drama',
-                'Fantasy',
-                'Film-Noir',
-                'Horror',
-                'Musical',
-                'Mystery',
-                'Romance',
-                'Sci-Fi',
-                'Thriller',
-                'War',
-                'Western']
+                      'Adventure',
+                      'Animation',
+                      "Children's",
+                      'Comedy',
+                      'Crime',
+                      'Documentary',
+                      'Drama',
+                      'Fantasy',
+                      'Film-Noir',
+                      'Horror',
+                      'Musical',
+                      'Mystery',
+                      'Romance',
+                      'Sci-Fi',
+                      'Thriller',
+                      'War',
+                      'Western']
         n_genre = len(all_genres)
 
         movies = {}
@@ -171,10 +175,10 @@ class Runner:
 
         users = {}
         for user_id_str, sex_str, age_str, occupation_str, zip_code in lines:
-                user_vec = np.zeros(1 + 1 + 21) # 1 categorical, 1 value, 21 categorical
-                user_vec[0] = 0 if sex_str == 'M' else 1 # sex
-                user_vec[1] = ages.index(int(age_str)) # age group (1, 18, ...)
-                user_vec[2+int(occupation_str)] = 1 # occupation (1-of-21)
+                user_vec = np.zeros(1 + 1 + 21)  # 1 categorical, 1 value, 21 categorical
+                user_vec[0] = 0 if sex_str == 'M' else 1  # sex
+                user_vec[1] = ages.index(int(age_str))  # age group (1, 18, ...)
+                user_vec[2 + int(occupation_str)] = 1  # occupation (1-of-21)
                 users[int(user_id_str)] = user_vec
 
         return users
@@ -188,12 +192,12 @@ class Runner:
             lines = map(lambda l: map(int, l.rstrip().split('::')), f.readlines())
             for l in lines:
                 # Since we consider positive-only feedback setting, ratings < 5 will be excluded.
-                if l[2] == 5: ratings.append(l)
+                if l[2] == 5:
+                    ratings.append(l)
         self.ratings = np.asarray(ratings)
 
         # sorted by timestamp
-        self.ratings = self.ratings[np.argsort(self.ratings[:,3])]
-
+        self.ratings = self.ratings[np.argsort(self.ratings[:, 3])]
 
     def __delta(self, d1, d2, opt='d'):
         """Compute difference between given 2 dates in month/day.
@@ -224,6 +228,7 @@ import click
 models = ['baseline', 'iMF', 'biased-iMF', 'iFMs', 'all_MF']
 contexts = ['dt', 'genre', 'demographics']
 
+
 @click.command()
 @click.option('--model', type=click.Choice(models), default=models[0], help='Choose a factorization model')
 @click.option('--context', '-c', type=click.Choice(contexts), multiple=True, help='Choose contexts used by iFMs')
@@ -246,7 +251,7 @@ def cli(model, context, limit):
         elif model == 'biased-iMF':
             avgs, time = exp.biased_iMF()
         elif model == 'iFMs':
-            model = model + '_' + '-'.join(context) # update output filename depending on contexts
+            model = model + '_' + '-'.join(context)  # update output filename depending on contexts
             avgs, time = exp.iFMs(context)
 
         save('results/%s.txt' % model, avgs, time)
